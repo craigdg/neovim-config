b4b21e47d48c8b9b0ec4c95f198a5dfbdb6ef7f7
craigdg <craigdgt-dev@protonmail.com>
2025-01-24 20:24:13 -0500

    feat: update terminal settings

--- lua/panes/source/floatingTerminal.lua
+++ lua/panes/source/floatingTerminal.lua
@@ -1,11 +1,14 @@
 local Terminal = require('toggleterm.terminal').Terminal
+
 local floatingTerminal = Terminal:new({
     direction = "float",
+    border = "shadow",
+    shade_terminals = true,
     on_open = function()
-        vim.cmd("startinsert!")
+        vim.cmd("startinsert")
     end,
     on_close = function()
-        vim.cmd("startinsert!")
+        vim.cmd("startinsert")
     end,
 })
 
